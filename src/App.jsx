d5ec7876dc476cb0f0a53a51e7e4d94da8cae92f
sabitameem <sabitameem578@gmail.com>
2023-04-06 19:42:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <div>
+      {/* header */}
+      <Header></Header>
+      <Outlet></Outlet>
+      {/* footer */}
+    </div>
+  );
+};
+
+export default App;
